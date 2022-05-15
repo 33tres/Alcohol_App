afbc44d39208756db6623369ef0b53ad3b43d1e8
davh <davh@umich.edu>
2022-05-15 15:50:24 -0400

    Made navigation views

--- Alcohol-App/ContentView.swift
+++ Alcohol-App/ContentView.swift
@@ -9,64 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        var Search_view = false
-        if (Search_view == true){
-            SearchView()
-        }
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.brown, .white]), startPoint: .top, endPoint: .bottom)
+        NavigationView{
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [.brown, .white]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-            VStack{
-                HStack{
-                    Button {
-                        Search_view = true
-                    } label: {
-                        Image(systemName: "globe")
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 125, height: 125)
-                            .padding(30)
-                    }
-
+                VStack{
                     HStack{
-                        Image(systemName: "person.3")
+                        NavigationLink(destination: SearchView()){
+                            Image(systemName: "globe")
                             .renderingMode(.original)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 125, height: 125)
                             .padding(30)
+                        }
+                        HStack{
+                            NavigationLink(destination: SearchView()){
+                                Image(systemName: "person.3")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 125, height: 125)
+                                .padding(30)
+                            }
+                        }
+                    }
+                    HStack{
+                        NavigationLink(destination: SearchView()){
+                            Image(systemName: "bolt.horizontal.circle")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 300)
+                            .padding(60)
+                        }
+                    }
+                    HStack{
+                        NavigationLink(destination: SearchView()){
+                            Image(systemName: "house")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                            .padding(15)
+                        }
+                        NavigationLink(destination: SearchView()){
+                            Image(systemName: "magnifyingglass")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                            .padding(15)
+                        }
+                        NavigationLink(destination: SearchView()){
+                            Image(systemName: "person")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                            .padding(15)
+                        }
                     }
                 }
-                HStack{
-                    Image(systemName: "bolt.horizontal.circle")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 200, height: 200)
-                        .padding(100)
-                }
-                HStack{
-                    Image(systemName: "house")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 75, height: 75)
-                        .padding(15)
-                    Image(systemName: "magnifyingglass")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 75, height: 75)
-                        .padding(15)
-                    Image(systemName: "person")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 75, height: 75)
-                        .padding(15)
-                }
-                
             }
         }
     }
