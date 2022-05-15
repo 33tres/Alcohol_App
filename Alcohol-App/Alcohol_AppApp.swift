882fcbcb5fbda9a5201a873b48deb10b1e757257
Sam Harrell <samzh16@gmail.com>
2022-05-15 13:57:01 -0400

    Initial Commit

---
+++ Alcohol-App/Alcohol_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alcohol_AppApp.swift
+//  Alcohol-App
+//
+//  Created by Samuel Harrell on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Alcohol_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
