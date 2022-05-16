1320b419a7348eaaa4f15bcdfa4ea3fa314e08bc
Sam Harrell <samzh16@gmail.com>
2022-05-16 13:17:47 -0400

    Friends list

--- Alcohol-App/FriendsView.swift
+++ Alcohol-App/FriendsView.swift
@@ -8,10 +8,140 @@
 import SwiftUI
 
 struct FriendsView: View {
+    let data = (1...50).map {
+        "Friend \($0)" }
+    let image =  Image(systemName: "person")
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    let customGreen = Color(red: 0.70, green: 0.81, blue: 0.67)
+    let customLightGreen = Color(red: 0.750, green: 0.85, blue: 0.67)
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            HStack{
+                Text("Friends List")
+                    .font(.title).bold().underline()
+            }
+            
+            LazyVGrid(columns: columns, spacing: 40) {
+                
+                ForEach(data, id: \.self) { item in
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .aspectRatio(contentMode:.fit)
+                        .padding(15)
+                        .background(customLightGreen)
+                    Text(item)
+                        .frame(width: 100, height: 30)
+                        .background(customLightGreen)
+                        .font(.title2)
+                        .padding(15)
+                    
+                }
+                
+            }
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(customGreen)
     }
 }
+    
+    
+    
+            
+       /*     }
+            HStack{
+                Text("Friends List")
+                    .font(.title).bold().underline()
+                    .frame(width: 200, height: 100)
+            }
+        VStack{
+            VStack{
+            
+            HStack{
+            Image(systemName: "person")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+            Text("friend 1")
+                .frame(width: 70, height: 25)
+            
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 2")
+                    .frame(width: 70, height: 25)
+                
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 3")
+                    .frame(width: 70, height: 25)
+                
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 4")
+                    .frame(width: 70, height: 25)
+                
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 5")
+                    .frame(width: 70, height: 25)
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 6")
+                    .frame(width: 70, height: 25)
+                
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 7")
+                    .frame(width: 70, height: 25)
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 8")
+                    .frame(width: 70, height: 25)
+            }
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                Text("friend 9")
+                .frame(width: 70, height: 25)
+                    }
+                }
+            }*/
+
+
 
 struct FriendsView_Previews: PreviewProvider {
     static var previews: some View {
