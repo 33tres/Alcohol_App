58c04dd517d2ca09f88454fe67f713f6d80609a4
davh <davh@umich.edu>
2022-05-18 13:15:57 -0400

    Changed the friend view and the profile view

--- Alcohol-App/FriendsView.swift
+++ Alcohol-App/FriendsView.swift
@@ -15,149 +15,197 @@ struct FriendsView: View {
         GridItem(.adaptive(minimum: 100))
     ]
     let customGreen = Color(red: 0.70, green: 0.81, blue: 0.67)
-    let customLightGreen = Color(red: 0.750, green: 0.85, blue: 0.67)
+    let customBrown = Color(red: 0.85, green: 0.66, blue: 0.41)
+    let customgray = Color(red: 0.89, green: 0.87, blue: 0.71)
+    let customOlive = Color(red: 0.81, green: 0.80, blue: 0.08)
     let customRed = Color(red: 0.71, green: 0.30, blue: 0.18)
 
     var body: some View {
         ScrollView {
             
-                
-            
-            
-            ScrollView {
-                Text("Friends List")
-                    .font(.title).bold().underline()
-                    .foregroundColor(.white)
-                    .padding()
-                    
-                    
-                LazyVGrid(columns: columns, spacing: 30) {
-                    
+            VStack{
+                HStack{
+                    ScrollView {
+                        HStack{
+                            Text("Leaderboards")
+                                .font(.system(size: 16))
+                                .font(.title).bold()
+                                .foregroundColor(.white)
+                                .padding(.top)
+                                .padding(.bottom)
+
+                            HStack{
+                                Image(systemName: "bag.fill")
+                                    .padding(.top)
+                                    .padding(.bottom)
+                            }
+                        }
+                            
+                            
+                        LazyVGrid(columns: columns, spacing: 30) {
+                            
+                                
                         
-                
-                ForEach(data, id: \.self) { item in
-                    Image(systemName: "person")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .aspectRatio(contentMode:.fit)
-                        .foregroundColor(.white)
-                        .padding()
-                    Text(item)
+                        ForEach(data, id: \.self) { item in
+                            HStack{
+                                Image(systemName: "person")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .aspectRatio(contentMode:.fit)
+                                    .foregroundColor(.white)
+                                HStack{
+                                    Text(item)
+                                        .font(.system(size: 15))
+                                        .font(.title).bold()
+                                        .foregroundColor(.white)
+                                        .shadow(radius: 20)
+                                }
+                            }
+                        }
+                    }
+                    }
+                    .frame(width: 150, height: 300)
+                        .background(customgray)
+                        .cornerRadius(20)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .shadow(color: customgray.opacity(0.3), radius: 20, x: 0, y: 10)
+                        .padding(10)
+                    HStack{
+                        ScrollView {
+                            HStack{
+                                Text("Friend List")
+                                    .font(.system(size: 20))
+                                    .font(.title).bold()
+                                    .foregroundColor(.white)
+                                    .padding(.top)
+                                    .padding(.bottom)
+
+                                HStack{
+                                    Image(systemName: "plus")
+                                        .padding(.top)
+                                        .padding(.bottom)
+                                }
+                            }
+                                
+                                
+                            LazyVGrid(columns: columns, spacing: 30) {
+                                
+                                    
+                            
+                            ForEach(data, id: \.self) { item in
+                                HStack{
+                                    Image(systemName: "person")
+                                        .resizable()
+                                        .frame(width: 30, height: 30)
+                                        .aspectRatio(contentMode:.fit)
+                                        .foregroundColor(.white)
+                                    HStack{
+                                        Text(item)
+                                            .font(.system(size: 15))
+                                            .font(.title).bold()
+                                            .foregroundColor(.white)
+                                            .shadow(radius: 20)
+                                    }
+                                }
+                            }
+                        }
+                            
+                            
+                        }
+                        .frame(width: 150, height: 300)
+                            .background(customOlive)
+                            .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                            .shadow(color: customOlive.opacity(0.3), radius: 20, x: 0, y: 10)
+                            .padding(10)
+                    }
+                }
+                VStack{
+                    Text("'Friend's recent badge")
+                        .font(.system(size: 23))
                         .font(.title).bold()
                         .foregroundColor(.white)
                         .shadow(radius: 20)
-                       
-                        
-                    
+                        .padding(.bottom)
+                    HStack{
+                        Image(systemName: "globe.europe.africa")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 75, height: 75)
+                        .padding(.bottom)
+                        HStack{
+                            VStack{
+                                Text("20 Taquilas Drank")
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                                .shadow(radius: 20)
+                                .font(.system(size: 20))
+                                ProgressView(value: 1)
+                                    .frame(width: 150, height: 20)
+                                    .background(customRed)
+                                    .padding(.bottom)
+                                    .padding(.bottom)
+                                    .padding(.leading)
+                            }
+                        }
+                    }
                 }
-            }
-                
-                
-            }.frame(width: 300, height: 300)
+                .frame(width: 320, height: 210)
                 .background(customRed)
                 .cornerRadius(20)
                 .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
                 .shadow(color: customRed.opacity(0.3), radius: 20, x: 0, y: 10)
                 .padding(10)
-            
+                
+                VStack{
+                    VStack{
+                        ScrollView {
+                            HStack{
+                                Text("Friend Requests")
+                                    .font(.system(size: 25))
+                                    .font(.title).bold()
+                                    .foregroundColor(.white)
+                                    .padding(.top)
+                                    .padding(.bottom)
+                            }
+                                
+                                
+                            LazyVGrid(columns: columns, spacing: 50) {
+                                
+                            ForEach(data, id: \.self) { item in
+                                VStack{
+                                    HStack{
+                                        Image(systemName: "person")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                            .aspectRatio(contentMode:.fit)
+                                            .foregroundColor(.white)
+                                        HStack{
+                                            Text(item)
+                                                .font(.system(size: 15))
+                                                .font(.title).bold()
+                                                .foregroundColor(.white)
+                                                .shadow(radius: 20)
+                                        }
+                                    }
+                                }                            }
+                            }
+                        }
+                    }
+                }
+                .frame(width: 320, height: 130)
+                .background(customBrown)
+                .cornerRadius(20)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                .shadow(color: customBrown.opacity(0.3), radius: 20, x: 0, y: 10)
+                .padding(10)
+            }
         }
         .frame(maxWidth: .infinity)
         .background(customGreen)
     }
 }
-    
-    
-    
-            
-       /*     }
-            HStack{
-                Text("Friends List")
-                    .font(.title).bold().underline()
-                    .frame(width: 200, height: 100)
-            }
-        VStack{
-            VStack{
-            
-            HStack{
-            Image(systemName: "person")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-            Text("friend 1")
-                .frame(width: 70, height: 25)
-            
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 2")
-                    .frame(width: 70, height: 25)
-                
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 3")
-                    .frame(width: 70, height: 25)
-                
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 4")
-                    .frame(width: 70, height: 25)
-                
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 5")
-                    .frame(width: 70, height: 25)
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 6")
-                    .frame(width: 70, height: 25)
-                
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 7")
-                    .frame(width: 70, height: 25)
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 8")
-                    .frame(width: 70, height: 25)
-            }
-            HStack{
-                Image(systemName: "person")
-                    .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                Text("friend 9")
-                .frame(width: 70, height: 25)
-                    }
-                }
-            }*/
-
 
 
 struct FriendsView_Previews: PreviewProvider {
--- Alcohol-App/ProfileView.swift
+++ Alcohol-App/ProfileView.swift
@@ -10,29 +10,60 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         let customGreen = Color(red: 0.70, green: 0.81, blue: 0.67)
-        let customLightGreen = Color(red: 0.750, green: 0.85, blue: 0.67)
         let customRed = Color(red: 0.71, green: 0.30, blue: 0.18)
         
-        
         VStack {
+            HStack{
             ScrollView{
-             
-                    
-                       
+
                     HStack{
-                        Image(systemName: "bag.fill")
+                        Image(systemName: "person.fill")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 75, height: 75)
                         .padding(15)
-                   
-                    } .frame(width: 200, height: 200)
+                        .frame(width: 100, height: 100)
                         .background(customRed)
                         .cornerRadius(20)
                         .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
                         .shadow(color: customRed.opacity(0.3), radius: 20, x: 0, y: 10)
-                        .padding(10)
+                        .padding(.trailing)
+                        .padding(.trailing)
+                        .padding(.trailing)
+                        HStack{
+                            VStack{
+                            Text("Username")
+                                .font(.system(size: 15))
+                                .font(.title).bold()
+                                .foregroundColor(.white)
+                                .shadow(radius: 20)
+                                .padding(5)
+                                VStack{
+                                Text("Email address")
+                                    .font(.system(size: 15))
+                                    .font(.title).bold()
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 20)
+                                    .padding(5)
+                                }
+                                VStack{
+                                Text("Badge Display")
+                                    .font(.system(size: 15))
+                                    .font(.title).bold()
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 20)
+                                    .padding(5)
+                                    .frame(width: 200, height: 50)
+                                    .background(customRed)
+                                    .cornerRadius(20)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                                    .shadow(color: customRed.opacity(0.3), radius: 20, x: 0, y: 10)
+                                }
+                            }
+                        }
+                    }
+                
                     
                 HStack{
                     
@@ -45,7 +76,7 @@ struct ProfileView: View {
                    
                     
                 }
-                .frame(width: 200, height: 200)
+                .frame(width: 300, height: 500)
                     .background(customRed)
                     .cornerRadius(20)
                     .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
@@ -58,14 +89,18 @@ struct ProfileView: View {
             .frame(maxWidth: .infinity)
         .background(customGreen)
            
+            }
         }
         }
     func showBeer() {
-        
 
     }
-      func showVodka() { }
-      func showTequila() { }
+    func showVodka() {
+        
+    }
+    func showTequila() {
+        
+    }
         }
        
         
